32eca52ddcc54c0dcb2d08c6c0a7109db2889905
ronaldStrong <ronaldstrong1993@gmail.com>
2019-02-13 22:58:32 -0500

    Update main.js

--- PRY/js/main.js
+++ PRY/js/main.js
@@ -9,7 +9,77 @@ window.onload = function() {
     // You will need to change the paths you pass to "game.load.image()" or any other
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
+    desktop = function (game, image, gameWorldMinX, gameWorldMinY, gameworldMaxX, gameWorldMaxY) {
+
+        //  We call the Phaser.Sprite passing in the game reference
+        //  We're giving it a random X/Y position here, just for the sake of this demo - you could also pass the x/y in the constructor
+        Phaser.Sprite.call(this, game, gameWorldx, gameWorldy, image);
+        var border = game.add.graphics(gameWorldx,gameWorldy);
+        var MinX=gameWorldMinX;
+        var MaxX=gameworldMaxX;
+        var MinY=gameWorldMinY;
+        var MaxY=gameWorldMaxY;
+        this.anchor.setTo(0.5, 0.5);
+        
+        game.add.existing(this);
+        var icons= [];
+        var iconsIndex=0;
+        var apps =[];
+        var appsIndex=0;
+        function startApp(app) {
+            apps[appsIndex]=app;
+            appsIndex++;
+            //add task box
+        }
+        function endApp(app){
+            
+        }
+    };
+    desktop.prototype = Object.create(Phaser.Sprite.prototype);
+    desktop.prototype.constructor = desktop;
     
+    desktop.prototype.update = function() {
+    
+        //  Automatically called by World.update
+        
+    
+    };
+    app = function (game, image, gameWorldMinX, gameWorldMinY, gameworldMaxX, gameWorldMaxY) {
+
+        //  We call the Phaser.Sprite passing in the game reference
+        //  We're giving it a random X/Y position here, just for the sake of this demo - you could also pass the x/y in the constructor
+        Phaser.Sprite.call(this, game, gameWorldx, gameWorldy, image, desktop);
+        var border = game.add.graphics(gameWorldx,gameWorldy);
+        var MinX=gameWorldMinX;
+        var MaxX=gameworldMaxX;
+        var MinY=gameWorldMinY;
+        var MaxY=gameWorldMaxY;
+
+        border.beginFill(0xFFFFFF);
+        border.lineStyle(4, 0xC0C0C0, 1);
+    
+        border.moveTo(gameWorldMinX, gameWorldMinY);
+        border.lineTo(gameworldMaxX, gameWorldMinY);
+        border.lineTo(gameworldMaxX, gameWorldMaxY);
+        border.lineTo(gameWorldMinX, 100);
+        border.lineTo(0, 200);
+        border.lineTo(0, 0);
+        border.endFill();
+
+        border.addChild(this);
+        this.anchor.setTo(0.5, 0.5);
+        
+        game.add.existing(this);
+    };
+    app.prototype = Object.create(Phaser.Sprite.prototype);
+    app.prototype.constructor = app;
+    
+    app.prototype.update = function() {
+    
+        //  Automatically called by World.update
+        
+    
+    };
     var game = new Phaser.Game( 960, 644, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
@@ -26,7 +96,7 @@ window.onload = function() {
         cow=game.add.sptite(700,350,'cow');
         cow.alignIn(desktop, Phaser.BOTTOM_RIGHT);            
         game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-        fskey = game.input.keyboard.addKey(Phaser.Keyboard.F10);
+        fskey = game.input.keyboard.addKey(Phaser.Keyboard.F11);
         fskey.onDown.add(gofull, this);        
         
     }
